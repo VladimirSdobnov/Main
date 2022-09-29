ddbfdbbd56ba23c81480d5e961261ddd00eeefb6
VladimirSdobniv <itmm-2213-a@E.local>
2022-09-29 08:39:58 +0300

    Ининциализация и вывод массива

--- TerminalClass/arraysacyion_easy/arraysacyion_easy.c
+++ TerminalClass/arraysacyion_easy/arraysacyion_easy.c
@@ -4,5 +4,16 @@
 #include <math.h>
 
 int main() {
+	srand(time(0));
 
+	int mass[10000000];
+	int size;
+
+	printf("Enter massive size (less 100000): ");
+	scanf_s("%d", &size);
+	for (int i = 0, i < size, i++) {
+		mass[i] = -10 + rand() % 10;
+		printf("%d ", mass[i]);
+	}
+	return 1;
 }
